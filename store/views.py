7db5902bbe4d01a727ba0ace2e7ac0e46c1c4e08
vlcp197 <vlcp197@gmail.com>
2024-07-31 20:08:04 -0300

    Add about, login and logout views

--- store/views.py
+++ store/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages 
 from . import models
 
 def home(request):
     products = models.Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
+def about(request):
+    return render(request, 'about.html',)
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user=authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Você logou com sucesso"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Ocorreu um erro, tente novamente"))
+            return redirect('home')
+    return render(request, 'login.html',)
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Você deslogou"))
+    return redirect("home")
