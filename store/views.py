fca9038a013eee8fdac964158e1e58bea247beb3
vlcp197 <vlcp197@gmail.com>
2024-08-01 20:35:44 -0300

    Add views for new added urls

--- store/views.py
+++ store/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
 from . import models
+from . import forms
+
+def product(request, pk):
+    product = models.Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
+
 
 def home(request):
     products = models.Product.objects.all()
@@ -23,9 +30,27 @@ def login_user(request):
         else:
             messages.success(request, ("Ocorreu um erro, tente novamente"))
             return redirect('home')
-    return render(request, 'login.html',)
+    else:
+        return render(request, 'login.html',)
 
 def logout_user(request):
     logout(request)
     messages.success(request, ("Você deslogou"))
     return redirect("home")
+
+def register_user(request):
+    form = forms.SignUpForm()
+    if request.method == "POST":
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Bem vindo. Você se registrou com sucesso"))
+            return redirect("home")
+        else:
+            messages.success(request, ("Houve um problema com o registro. Por favor, tente novamente"))
+            return redirect("home")
+    return render(request, 'register.html', {'form':form })
