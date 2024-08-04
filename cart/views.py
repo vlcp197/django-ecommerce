343561e6e64a9f23d98cd1755f8c664b50bd9bff
vlcp197 <vlcp197@gmail.com>
2024-08-04 19:39:26 -0300

    Change behavior for car views to add quantities

--- cart/views.py
+++ cart/views.py
@@ -4,16 +4,20 @@ from store.models import Product
 from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    cart = Cart(request)
+    cart_products = cart.get_products
+    quantities = cart.get_quantities
+    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities": quantities})
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
-
-        response = JsonResponse({"Product Name: ": product.name})
+        cart.add(product=product, quantity=product_qty)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({"qty: ": cart_quantity})
         return response
 
 
