aaabd0cf565944daa17a1a687c8d8e13eacb7ee3
vlcp197 <vlcp197@gmail.com>
2024-09-01 19:04:16 -0300

    Add new model profile to be available on admin user page

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.models import User
 
 admin.site.register(models.Category)
 admin.site.register(models.Customer)
 admin.site.register(models.Product)
 admin.site.register(models.Order)
+admin.site.register(models.Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email", ]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
