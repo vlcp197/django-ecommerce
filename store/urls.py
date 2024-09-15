ecf393462000ce0650791bc98b59993da8610b28
vlcp197 <vlcp197@gmail.com>
2024-09-15 20:18:34 -0300

    Minor refactoring

--- store/urls.py
+++ store/urls.py
@@ -15,4 +15,4 @@ urlpatterns = [
     path("category/<str:foo>", views.category, name="category"),
     path("category_summary/", views.category_summary, name="category_summary"),
     path("search/", views.search, name="search"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
