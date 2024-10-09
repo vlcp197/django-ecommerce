49bd203bd26ca7e0395807d11fba064a5d059115
vlcp197 <vlcp197@gmail.com>
2024-10-08 22:09:20 -0300

    Build checkout view

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from cart.cart import Cart
+
+
+def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_products
+    quantities = cart.get_quantities
+    totals = cart.cart_total()
+    return render(request, "payment/checkout.html", {"cart_products": cart_products, "quantities": quantities, "totals":totals})
+
 
 def payment_success(request):
     return render(request, "payment/payment_success.html", {})
