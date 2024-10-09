a6c673cd77401044c3cd7d58aadfd69096b3da24
vlcp197 <vlcp197@gmail.com>
2024-10-09 20:31:20 -0300

    Add function to create shipping information when registering a new user

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -19,6 +20,14 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
     
+# Create a user Shipping Address by default when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+post_save.connect(create_shipping, sender=User)
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=250)
