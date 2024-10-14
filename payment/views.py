95500a0063913ae9e0176688b4e1e50aec473bc2
vlcp197 <vlcp197@gmail.com>
2024-10-14 20:29:06 -0300

    Build view for billing_info page

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
+
+def billing_info(request):    
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_products
+        quantities = cart.get_quantities
+        totals = cart.cart_total()
+
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals":totals, "shipping_info": request.POST, "billing_form":billing_form})
+        else:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals":totals, "shipping_info": request.POST, "billing_form":billing_form})
+
+        shipping_form = request.POST
+        return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals":totals, "shipping_form": shipping_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
 
 def checkout(request):
     cart = Cart(request)
@@ -18,8 +40,6 @@ def checkout(request):
         # Checkout as guest
         shipping_form = ShippingForm(request.POST or None)
         return render(request, "payment/checkout.html", {"cart_products": cart_products, "quantities": quantities, "totals":totals})
-
-
 
 
 def payment_success(request):
