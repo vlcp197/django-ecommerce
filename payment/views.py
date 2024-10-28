9987747e17007fbef30bdc21c3d364b108ca6614
vlcp197 <vlcp197@gmail.com>
2024-10-27 21:10:21 -0300

    Build process order view

--- payment/views.py
+++ payment/views.py
@@ -1,15 +1,84 @@
 from django.shortcuts import redirect, render
 from cart.cart import Cart
 from payment.forms import ShippingForm, PaymentForm
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 from django.contrib import messages
+from store.models import Product
+
+
+def process_order(request):
+    if request.POST:
+        
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        
+        
+        payment_form = PaymentForm(request.POST or None)
+        my_shipping = request.session.get('my_shipping')
+        
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}\n"
+        amount_paid = totals  
+        
+        if request.user.is_authenticated:
+            
+            user = request.user
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid )
+            create_order.save()
+
+            order_id = create_order.pk
+
+            for product in cart_products():
+                product_id = product.id
+
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+            
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    del request.session[key]
+
+            
+            messages.success(request, "Pedido Incluido")
+            return redirect("home")
+        else:
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid )
+            create_order.save()
+
+        for key in list(request.session.keys()):
+                if key == "session_key":
+                    del request.session[key]
+
+        messages.success(request, "Pedido Incluido")
+        return redirect("home")
+        
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
 
 def billing_info(request):    
+    
     if request.POST:
         cart = Cart(request)
         cart_products = cart.get_products
         quantities = cart.get_quantities
         totals = cart.cart_total()
+
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
 
         if request.user.is_authenticated:
             billing_form = PaymentForm()
