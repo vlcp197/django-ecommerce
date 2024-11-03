615eda0b01de018bb903d1ee5b9d2d109002af85
vlcp197 <vlcp197@gmail.com>
2024-11-03 17:05:28 -0300

    Build views and pages for orders dashboard and shipped and not shipped pages

--- payment/views.py
+++ payment/views.py
@@ -6,6 +6,31 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from store.models import Product
 
+def orders(request, pk):
+    if request.user.is_authenticated and request.user.is_superuser:
+        order = Order.objects.get(id=pk)
+        items = OrderItem.objects.filter(order=pk)
+        return render(request, 'payment/orders.html', {"order": order, "items": items})
+    else:
+        messages.success(request, "Acesso negado")
+        return redirect("Home")
+
+def not_shipped_dash(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+        orders = Order.objects.filter(shipped=False)
+        return render(request, "payment/not_shipped_dash.html",{"orders": orders})
+    else:
+        messages.success(request, "Acesso negado")
+        return redirect("Home")
+
+def shipped_dash(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+        orders = Order.objects.filter(shipped=True)
+        return render(request, "payment/shipped_dash.html",{"orders": orders})
+    else:
+        messages.success(request, "Acesso negado")
+        return redirect("home")
+
 
 def process_order(request):
     if request.POST:
