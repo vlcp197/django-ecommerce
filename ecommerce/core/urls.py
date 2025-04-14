15b8afb745721ddc92bee3712bbfb9a7460735b4
vpadilha <vpadilha@wevets.com.br>
2025-04-13 21:40:13 -0300

    Add product page and cart page

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('produto/<int:product_id>/', views.product_detail, name="product-detail"),
+    path('carrinho/', views.cart, name="cart"),
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-
 def home(request):
     return render(request, 'core/home.html', {})
+
+def product_detail(request, product_id):
+    return render(request, 'core/product_detail.html', {"product_id": product_id})
+
+def cart(request):
+    return render(request, 'core/cart.html')
