fd8fe35705126be824165eb382eb41ec2acf9af5
vlcp197 <vlcp197@gmail.com>
2025-04-15 20:05:48 -0300

    add objects to database and to templates

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.utils.text import slugify
 
-class Product:
-    name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
-    description = models.TextField()
-    image = models.ImageField()
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100,  blank=True, null=True)
+    slug = models.SlugField(max_length=100,  blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100,  blank=True, null=True)
+    slug = models.SlugField(max_length=100,  blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)],blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,3 +38,10 @@ class Product:
 
     def __str__(self):
         return f"{self.name}"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
-    return render(request, 'core/home.html', {})
+    products = Product.objects.all()
+    return render(request, 'core/home.html', {'products': products})
 
 def product_detail(request, product_id):
-    return render(request, 'core/product_detail.html', {"product_id": product_id})
+
+    product = Product.objects.get(id=product_id)
+    return render(request, 'core/product_detail.html', {"product": product})
 
 def cart(request):
     return render(request, 'core/cart.html')
