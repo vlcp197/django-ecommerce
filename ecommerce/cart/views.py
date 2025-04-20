9af00ae083c9f20d6d69276ee4bf7798fca3921a
vlcp197 <vlcp197@gmail.com>
2025-04-19 23:22:30 -0300

    Add methods to cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,7 +1,9 @@
 import random
 import string
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
 from core.models import Product
+
 
 def cart(request):
 
@@ -12,14 +14,35 @@ def generate_order_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
 def add_to_cart(request, slug):
-    product = get_object_or_404(Product)
-    ...
+    product = get_object_or_404(Product, slug=slug)
+    cart = request.session.get('cart', {})
 
-def remove_from_cart():
-    ...
+    if slug in cart:
+        cart[slug]["quantity"] += 1
+    else:
+        cart[slug] = {
+            'quantity': 1,
+            'preco_unitario': float(product.price)
+        }
+    
+    request.session['cart'] = cart
+    messages.success(request, f"{product.name} adicionado ao carrinho")
+    return redirect('core.home')
 
-def update_cart():
-    ...
+def remove_from_cart(request, slug):
+    cart = request.session.get('cart', {})
+
+    if slug in cart:
+        del cart[slug]
+        request.session["cart"] = cart 
+        messages.success(request, "Item removido do carrinho")
+
+    return redirect('cart')
+
+
+def update_cart(request):
+    if request.method == 'POST':
+        ...
 
 def checkout_view():
     ...
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -121,3 +121,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
